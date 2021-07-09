1520283fd883c96109b56dfbdaeeb69fe2a8bcbf
AjaeOnTheRun <syahrul.azrai@gmail.com>
2021-07-09 21:49:50 +0800

    update

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,33 +4,29 @@
       <div class="flex justify-between">
         <div class="text-3xl">
           <nuxt-link to="/">
-            <div>
-              <h1>Creative<span class="font-bold">Khayr</span>Studio</h1>
-            </div>
+            <h1>Creative<span class="font-bold">Khayr</span>Studio</h1>
           </nuxt-link>
         </div>
 
         <div class="my-auto">
           <nuxt-link to="/students"
-            ><a class="py-2 px-3 ml-2 hover:bg-indigo-500 rounded">
-              students
-            </a></nuxt-link
+            ><p class="py-2 px-3 ml-2 hover:bg-indigo-500 rounded">students</p></nuxt-link
           >
           <nuxt-link to="/challenge"
-            ><a class="py-2 px-3 ml-2 hover:bg-indigo-500 rounded">
+            ><p class="py-2 px-3 ml-2 hover:bg-indigo-500 rounded">
               challenge
-            </a></nuxt-link
+            </p></nuxt-link
           >
-          <!-- <nuxt-link to="/gallery"><a class="py-2 px-3 ml-2 hover:bg-indigo-500 rounded">
-             gallery 
-          </a></nuxt-link> -->
+          <nuxt-link to="/gallery"
+            ><p class="py-2 px-3 ml-2 hover:bg-indigo-500 rounded">gallery</p></nuxt-link
+          >
 
           <nuxt-link to="/reportCard"
-            ><a
+            ><p
               class="py-2 px-3 ml-2 hover:bg-indigo-400 bg-indigo-500 rounded shadow-lg text-white"
             >
               Report Card
-            </a></nuxt-link
+            </p></nuxt-link
           >
         </div>
       </div>
